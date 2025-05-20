1aeb0387985d02b089e3ad30db65209e37330439
Gautam Thakur <117568564+gautamthakur08@users.noreply.github.com>
2025-05-20 20:45:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/ssb" element={<Home />} />
         <Route path="/ppdt" element={<PPDTTest />} />
         <Route path="/wat" element={<WATTest />} />
       </Routes>
