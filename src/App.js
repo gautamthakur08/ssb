1f9d9ebbbd45a3b53823e88d37108e383950b7fd
Gautam Thakur <117568564+gautamthakur08@users.noreply.github.com>
2025-05-20 20:50:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import PPDTTest from './components/tests/PPDT/PPDTTest.jsx';
 import WATTest from './components/tests/WAT/WATTest.jsx';
 import Home from './pages/Home';
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/ssb" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/ppdt" element={<PPDTTest />} />
         <Route path="/wat" element={<WATTest />} />
       </Routes>
